3e1108689ccacb7a0d0fdb23805e84191ddc06bb
reximer <reximer@yuwin.co.kr>
2013-09-14 16:39:04 +0900

    ddd

--- js/app.js
+++ js/app.js
@@ -4,10 +4,10 @@
 // Declare app level module which depends on filters, and services
 angular.module('myApp', ['myApp.filters', 'myApp.services', 'myApp.directives']).
   config(['$routeProvider', function($routeProvider) {
-    $routeProvider.when('/home', {templateUrl: 'partials/home.html', controller: GenericViewCtrl});
-    $routeProvider.when('/project_a', {templateUrl: 'partials/project_a.html', controller: GenericViewCtrl});
-    $routeProvider.when('/project_b', {templateUrl: 'partials/project_b.html', controller: GenericViewCtrl});
+    $routeProvider.when('/intro', {templateUrl: 'partials/intro.html', controller: GenericViewCtrl});
+    $routeProvider.when('/tech_a', {templateUrl: 'partials/tech_a.html', controller: GenericViewCtrl});
+    $routeProvider.when('/tech_b', {templateUrl: 'partials/tech_b.html', controller: GenericViewCtrl});
+    $routeProvider.when('/tech_c', {templateUrl: 'partials/tech_c.html', controller: GenericViewCtrl});
     $routeProvider.when('/contact', {templateUrl: 'partials/contact.html', controller: ContactViewCtrl});
-    $routeProvider.when('/imprint', {templateUrl: 'partials/imprint.html', controller: GenericViewCtrl});
-    $routeProvider.otherwise({redirectTo: '/home'});
+    $routeProvider.otherwise({redirectTo: '/intro'});
   }]);
